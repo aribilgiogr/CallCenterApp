68f487043707afdb8b79ebeac35a75af95612095
aribilgiogr <aribilgiogr@gmail.com>
2026-04-13 11:34:33 +0300

    13.04.2026 Update 1

---
+++ Core/Concretes/Entities/ApplicationUser.cs
@@ -0,0 +1,66 @@
+﻿using Core.Abstracts.Bases;
+using Core.Concretes.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Concretes.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastLoginDate { get; set; }
+        public bool IsDeleted { get; set; } = false;
+
+        public virtual ICollection<Customer> Customers { get; set; } = [];
+    }
+
+    public class Customer : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+        public string? TaxNumber { get; set; }
+        public string? Industury { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+
+        [ForeignKey(nameof(AssignedUser))]
+        public string? AssignedUserId { get; set; }
+        public virtual ApplicationUser? AssignedUser { get; set; }
+        public bool IsPerson { get; set; }
+        public CustomerStatus Status { get; set; } = CustomerStatus.Potential;
+
+        public virtual ICollection<Contact> Contacts { get; set; } = [];
+    }
+
+    public class Activity : BaseEntity
+    {
+        [Required]
+        public string Subject { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? CompletedAt { get; set; }
+        public bool IsCompleted { get; set; } = false;
+
+        [ForeignKey(nameof(AssignedUser))]
+        public string? AssignedUserId { get; set; }
+        public virtual ApplicationUser? AssignedUser { get; set; }
+
+        [ForeignKey(nameof(RelatedCustomer))]
+        public string? RelatedCustomerId { get; set; }
+        public virtual Customer? RelatedCustomer { get; set; }
+
+        public ActivityType Type { get; set; }
+    }
+}
---
+++ Core/Concretes/Enums/CustomerStatus.cs
@@ -0,0 +1,5 @@
+﻿namespace Core.Concretes.Enums
+{
+    public enum CustomerStatus { Potential = 1, Active = 2, Inactive = 3, Lost = 4 }
+    public enum ActivityType { Call = 1, Email = 2, Meeting = 3, Task = 4, Note = 5, Demo = 6, FollowUp = 7 }
+}
